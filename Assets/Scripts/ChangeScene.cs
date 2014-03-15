34dad41e2955d5a302f024fd8a9501cdae2de753
Andrew Gorman <a.c.gorman@wustl.edu>
2014-03-15 12:48:46 +0000

    Committed work so far

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string nextSceneName;
+	public string debugText;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonUp (0)) {
+			Debug.Log (debugText);
+			Application.LoadLevel(nextSceneName);
+		}
+	}
+}
